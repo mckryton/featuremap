de02d8bb83ae452b2b2a3305cb9932d581533c8f
Matthias Carell <m_carell@yahoo.de>
2018-04-21 15:26:42 +0200

    todos updated

--- features/support/validate_xml.rb
+++ features/support/validate_xml.rb
@@ -1,5 +1,3 @@
-require 'nokogiri'
-
 def validate_mm(p_featuremap_path)
   schema = Nokogiri::XML::Schema(File.read("doc/definitions/freemind.xsd"))
   document = Nokogiri::XML(File.read(p_featuremap_path))
