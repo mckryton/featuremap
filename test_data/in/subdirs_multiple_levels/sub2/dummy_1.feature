9510f94dc450f558fe78728159ba21a2debc4a7d
Matthias Carell <m_carell@yahoo.de>
2019-06-02 17:13:26 +0200

    remove automatically created test data from repository

--- test_data/in/subdirs_multiple_levels/sub2/dummy_1.feature
+++
@@ -1,7 +0,0 @@
-Feature: dummy feature for testing
-  This is a dummy just for testing purposes
-
-  Scenario: feature containing a scenario
-    Given something
-    When action
-    Then result
--- test_data/out/featuremap.mm
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1529829126" ID="ID_748a6aca65314396972fec00a74ba118" MODIFIED="1529829126" TEXT="featuremap">
-<node CREATED="1529829126" ID="ID_47d7574a76b1422dbaa1c38fa6cc13b4" MODIFIED="1529829126" TEXT="dummy feature for testing">
-<icon BUILTIN="idea"/></node><node CREATED="1529829126" ID="ID_09256a884bb44f59b449caca7dc8f545" MODIFIED="1529829126" TEXT="dummy feature for testing">
-<icon BUILTIN="idea"/></node><node CREATED="1529829126" ID="ID_e7198cc60b9c4f90903c7418c98ab389" MODIFIED="1529829126" TEXT="dummy feature for testing">
-<icon BUILTIN="idea"/></node></node>
-</map>
